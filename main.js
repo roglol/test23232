589de36f0128f00b2eee691b5e2277f78154c723
otiko <otikopotskhoraia@gmail.com>
2018-06-16 16:38:12 +0400

    commit

---
+++ main.js
@@ -0,0 +1,181 @@
+// let num = prompt("Enter a numbers");
+
+// if ( num % 2 == 0) {
+//     alert("Even");
+//     alert("Have fun");
+// }
+// else {
+//      alert("odd");
+// }
+
+// if(!0) {
+//     alert("false")
+// }
+
+
+// let accessAllowed;
+// let age = prompt("age");
+// if( age > 18 ) {
+//     accessAllowed = true;
+// }
+// else {
+//     accessAllowed = false;
+// }
+// alert(accessAllowed);
+
+// //let result =  condition ? value_1 : value_2;
+// accessAllowed = (age > 18) ? true : false;
+
+// let login = prompt("Enter login") 
+// if (login == "Admin") {
+//     let password= prompt("Enter password")
+//     if ( password == "TheMaster"){ 
+//         alert("WELCOME")
+//    }
+//     else if( password == "" || password == null ){
+//            alert("canceled")
+//        }
+//        else { 
+//            alert( "wrong password")
+   
+//        }
+    
+// }
+// else if  (login == "" || login == null) 
+//         {alert("canceled") 
+// }
+
+// else {
+//         alert("I don't know you")
+// }
+
+// let hour = 11;
+// let minute = 44;
+// if (hour == 11 && minute == 44) {
+//     alert("time is 11:44");
+// }
+
+// if ( 1 && 0) {
+//     alert("wont work")
+// }
+
+// alert(1 && 5);
+// alert(null && 5);
+// alert( !5 || 1);
+ 
+
+// 
+// let year =  prompt("enter year")
+// if( year % 4 == 0 && year % 100 !== 0 ) {
+//     alert( "leap")
+// }
+ 
+//  else if (year % 400 == 0) {
+//      alert("leap")
+//  }
+//  else {
+//      alert("go home")
+//  }
+
+// let a = 4;
+
+// switch(a) {
+//  default: 
+//  alert("default");
+//  break;
+
+// }
+
+// let browser = prompt('enter browser name')
+// if (browser = 'Edge'){
+//   alert("you have edge")
+
+// }
+// else if (browser = 'Chrome' || 'Firefox' || 'Safari' || 'Opera' ) {
+//     alert('ok we suppot these browsers too')
+// }
+// else{
+//     alert('we hope that this page looks ok!')
+// }
+
+// let year = prompt('enter year')
+// if (year % 4 == 0 & (!(year % 100 == 4) || year % 400 == 0)) {
+//     alert("leap year")
+// }
+// else {
+//     alert("not a leap year")
+
+// }
+
+// let num1 = prompt('enter first number')
+// let num2 = prompt('enter second number')
+// let num3 = prompt("operator")
+
+// if(num3 == "+") {
+//     alert(parseInt(num1) + parseInt(num2));
+// }
+// else if(num3 == "-") {
+//     alert(num1 - num2);
+
+// }
+// else if(num3 == "*") {
+//     alert((num1 * num2));
+
+// }
+// else if(num3 == "/") {
+//     alert(num1 / num2);
+
+// }
+
+// let i = 0;
+// while ( i < 10 ) {
+//     document.write(i);
+//     i++;
+// }
+// document.write('<br>')
+// while(i) {
+//     document.writeln(i);
+//     i--;
+// }
+
+// while( i < 3) {
+//     alert(i++);
+// }
+
+// let i = 0
+// do {
+//     alert(i);
+//     i++;
+
+// } while( i < 4 )
+// num = Number(prompt("write number"))
+// let i = 0 
+// while (num != 0) {
+//     i = i + num%10
+//     num = (num - num%10)/10
+// }
+// alert(i)
+
+// let  x = prompt('enter number')
+// while(isNaN(x)) {
+//     x = prompt('enter number')
+// }
+
+// for( begin; condition; step) {
+//     code
+// }
+
+// for( let i = 0; i < 10; i++) {
+//     alert(i);
+// }
+
+// let x =Number(prompt('minimum number'))
+// let y =Number(prompt('maximum number'))
+// while (x<=y ) {
+//     if (x % 2== 0)  {
+//         document.write(x)
+//     }
+//     x++;
+// }
+
+
